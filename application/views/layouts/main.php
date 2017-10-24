1d9d09a33ab91338b2a9bec48b064422a7257a9d
João Paulo Claudino Silva <jpaulocs@gmail.com>
2017-10-23 22:43:09 -0200

    removendo diretorio de logs

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -35,7 +35,7 @@
                 <nav class="navbar navbar-static-top">
                     <!-- Sidebar toggle button-->
                     <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-                        <span class="sr-only">Toggle navigation</span>
+                        <span class="sr-only">Toggle navigation </span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
